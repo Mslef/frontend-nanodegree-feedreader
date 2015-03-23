d6641be7c9a5ab680f16a733f8c4b8078ebf8a6b
Mslef <marcel.lefebvre01@gmail.com>
2015-03-23 15:49:34 -0400

    Write tests for the jasmine spec file and update README to clarify future features

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -26,47 +26,151 @@ $(function() {
             expect(allFeeds.length).not.toBe(0);
         });
 
+        it('all have valid URL', function () {
+            // Loop through each feed in the allFeeds object and ensures it has a URL defined and that the URL is not empty.
+            
+            for (var i = 0; i <allFeeds.length; i++) {
+                expect(allFeeds[i].url).toBeDefined();
+                expect(allFeeds[i].url.length).not.toBe(0);
+                expect(typeof allFeeds[i].url).toBe("string");
+            }
+        });
+        
+        it('all have valid names', function () {
+            // Loop through each feed in the allFeeds object and ensures it has a name defined and that the name is not empty.
+            for (var i = 0; i <allFeeds.length; i++) {
+                expect(allFeeds[i].name).toBeDefined();
+                expect(allFeeds[i].name.length).not.toBe(0);
+                expect(typeof allFeeds[i].name).toBe("string");
+            }
+        });
+        
+    });
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
-         */
+    //Testing the menu
+    describe('The menu', function() {
+        var lefAlignValue, menuFontSize;
 
+        it('is hidden by default', function () {
+            // Test that the left position menu is -12 times its font size on load to hide it
+            lefAlignValue = Number($(".menu").css("transform").slice(19, -4));
+            menuFontSize = Number($(".menu").css('font-size').slice(0,-2));
+            expect(lefAlignValue<=-12*menuFontSize).toBe(true);
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
-         */
+        });
+
+        // Ensure the menu changes visibility when the menu icon is clicked
+        describe('when clicked,', function() {
+            
+            // Replicate the action of clicking the button and wait 1 second
+            beforeEach(function(done) {
+                $('body').toggleClass('menu-hidden');
+                setTimeout(function() {  
+                    done();
+                }, 1000);
+            }); 
+
+            // Test the menu display when clicked
+            it('becomes visible if it was hidden', function (done) {         
+                lefAlignValue = Number($(".menu").css("transform").slice(19, -4));   
+                expect(lefAlignValue).toBe(0);
+                done();
+            });
+
+            // Test the menu hides when it is clicked again
+            it('becomes invisible if it was visible', function (done) {
+                lefAlignValue = Number($(".menu").css("transform").slice(19, -4));
+                menuFontSize = Number($(".menu").css('font-size').slice(0,-2));
+                expect(lefAlignValue<=-12*menuFontSize).toBe(true);
+                done();    
+            });  
+        });
+    });
+
+    //Testing initial entries
+    describe('Initial Entries', function() {
+
+        //Randomly load a feed after 1 second
+        beforeEach(function(done) {
+                var randIndex = Math.floor(Math.random() * 4);
+                loadFeed(randIndex);
+                setTimeout(function() {  
+                    done();
+                }, 1000);
+            });
+
+        // Test that the .entry element contains at least 1 element, and that it is located inside the feed element
+        it('show at least one entry', function (done) {
+            expect($('.feed').children().length>=1).toBe(true);
+            expect($('.entry').length>=1).toBe(true);
+            done();
+        });
     });
 
 
-    /* TODO: Write a new test suite named "The menu" */
+    //Testing behavior while selecting new feed
+    describe('New Feed Selection', function() {
 
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
-         */
+        function getFeedContent (inputVariable) {
+            $('.entry').each(function(){
+                inputVariable.push($(this).text());
+            });
+        }
 
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
+        var feedContent = [], newFeedContent = []; 
+        
+        beforeEach(function(done) {
+                // Get the original feeds' contents
+                getFeedContent(feedContent);
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+                // randomly select a feed other than the default one
+                var randIndex = Math.floor(Math.random() * 4);
+                //console.log(randIndex);
+                loadFeed(randIndex);
+                setTimeout(function() {  
+                    done();
+                }, 1000);
+            });
 
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test wil require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
+        it('changes the content of the visible feed', function () {
 
-    /* TODO: Write a new test suite named "New Feed Selection"
+            //Check that the new content is different than feedContent
+            getFeedContent(newFeedContent);
+            for (var i =0; i < feedContent.length; i++) {
+                expect(feedContent[i] === newFeedContent[i]).toBe(false);
+            }
+          
+        });
+    }); 
 
-        /* TODO: Write a test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
-         */
+    //Extra tests
+    describe('New feeds', function() {
+
+        // Test new feeds are properly added to the end of allFeeds
+        it('can be added', function(){
+            addFeed('Test Feed','https://www.apple.com/ca/hotnews/feeds/ticker.rss');
+            expect(allFeeds[allFeeds.length-1].name).toBe('Test Feed');
+            expect(allFeeds[allFeeds.length-1].url).toBe('https://www.apple.com/ca/hotnews/feeds/ticker.rss');
+        });
+
+    });
+
+    describe('Flags', function() {
+
+        beforeEach(function(done) {
+            var randIndex = Math.floor(Math.random() * 4);
+            flagFeed(randIndex);
+            setTimeout(function() {  
+                done();
+            }, 1000);
+        });
+
+        // Test flags can be applied to feeds by adding the class flagged to feeds
+        it('can be applied to feeds', function(done){
+            expect($('.flagged').length >= 1).toBe(true);
+            expect($('.flagged').css('background-color')).toBe('05cc00');
+            done();
+        });
+    });
+   
 }());
